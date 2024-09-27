6d5f0457959c5593ff3b55daee64b112afb20b0e
JosueIsOffline <josuehernandez2314@gmail.com>
2024-09-27 16:17:55 -0400

    Creating a Dark Mode

--- app/index.tsx
+++ app/index.tsx
@@ -1,45 +1,74 @@
-import { Text, View, StyleSheet, TouchableOpacity, useColorScheme } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  useColorScheme,
+} from "react-native";
 import Icon from "@/assets/images/wordle-icon.svg";
 import { Link } from "expo-router";
-import { format } from 'date-fns'
+import { format } from "date-fns";
 import { Colors } from "@/constants/Colors";
+import ThemedText from "@/components/ThemedText";
 
 export default function Index() {
-  const colorScheme = useColorScheme()
-  const backgroundColor = Colors[colorScheme ?? 'light'].background
-  const textColor = Colors[colorScheme ?? 'light'].text
+  const colorScheme = useColorScheme();
+  const backgroundColor = Colors[colorScheme ?? "light"].background;
+  const textColor = Colors[colorScheme ?? "light"].text;
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor }]}>
       <View style={styles.header}>
         <Icon width={100} height={70} />
-        <Text style={styles.title}>Wordle</Text>
-        <Text style={styles.text}>Get 6 chances to guess a 5-letter word.</Text>
+        <ThemedText style={styles.title}>Wordle</ThemedText>
+        <ThemedText style={styles.text}>
+          Get 6 chances to guess a 5-letter word.
+        </ThemedText>
       </View>
 
       <View style={styles.menu}>
-        <Link href={"/game"} style={[styles.btn, {
-          backgroundColor: colorScheme === 'light' ? "#000" : "#4a4a4a"
-        }]} asChild>
+        <Link
+          href={"/game"}
+          style={[
+            styles.btn,
+            {
+              backgroundColor: colorScheme === "light" ? "#000" : "#4a4a4a",
+            },
+          ]}
+          asChild
+        >
           <TouchableOpacity>
             <Text style={[styles.btnText, styles.primaryText]}>Play</Text>
           </TouchableOpacity>
         </Link>
 
-        <TouchableOpacity style={styles.btn}>
-          <Text style={styles.btnText}>Log in</Text>
+        <TouchableOpacity
+          style={[
+            styles.btn,
+            { borderColor: textColor },
+          ]}
+        >
+          <ThemedText style={styles.btnText}>Log in</ThemedText>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.btn}>
-          <Text style={styles.btnText}>Suscribe</Text>
+        <TouchableOpacity
+          style={[
+            styles.btn,
+            { borderColor: textColor },
+          ]}
+        >
+          <ThemedText style={styles.btnText}>Suscribe</ThemedText>
         </TouchableOpacity>
-        
       </View>
 
       <View style={styles.footer}>
-        <Text style={styles.footerDate}>{format(new Date(), 'MMMM d, yyyy')}</Text>
-        <Text style={styles.footerText}>No. 1249</Text>
-        <Text style={styles.footerText}>Edited by Josue Hernandez</Text>
+        <ThemedText style={styles.footerDate}>
+          {format(new Date(), "MMMM d, yyyy")}
+        </ThemedText>
+        <ThemedText style={styles.footerText}>No. 1249</ThemedText>
+        <ThemedText style={styles.footerText}>
+          Edited by Josue Hernandez
+        </ThemedText>
       </View>
     </View>
   );
@@ -50,10 +79,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     paddingHorizontal: 50,
-    gap: 40
+    gap: 40,
   },
   header: {
-    alignItems: 'center',
+    alignItems: "center",
     gap: 10,
   },
   title: {
@@ -62,41 +91,40 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 26,
-    textAlign: 'center'
+    textAlign: "center",
   },
   menu: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     gap: 10,
   },
   footer: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   footerText: {
-    fontSize: 14
+    fontSize: 14,
   },
   footerDate: {
     fontSize: 14,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   btn: {
-    justifyContent: 'center',
+    justifyContent: "center",
     borderRadius: 30,
-    alignItems: 'center',
-    borderColor: '#000',
+    alignItems: "center",
+    borderColor: "#000",
     borderWidth: 1,
-    width: '60%',
-    maxWidth: 200
+    width: "60%",
+    maxWidth: 200,
   },
   btnText: {
     padding: 14,
     fontSize: 20,
-    fontWeight: 'semibold',
-    color: "#333"
+    fontWeight: "semibold",
+    color: "#333",
   },
   primaryText: {
-    color: "#fff"
-  }
-
+    color: "#fff",
+  },
 });
